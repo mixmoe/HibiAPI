83f99a5a9e5dac6ea1f1b44103ce24d48136b0f5
mix <admin@yami.im>
2021-01-02 20:15:59 +0800

    :white_check_mark: 添加Bilibili API的测试

---
+++ test/test_bilibili.py
@@ -0,0 +1,113 @@
+import pytest
+from app import app as APIAppRoot
+from fastapi.testclient import TestClient
+
+
+@pytest.fixture(scope="package")
+def client():
+    with TestClient(APIAppRoot, base_url="http://testserver/bilibili/v3/") as client:
+        yield client
+
+
+def test_video_info(client: TestClient):
+    response = client.get("video_info", params={"aid": 2})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_video_address(client: TestClient):
+    response = client.get(
+        "video_address",
+        params={"aid": 2, "cid": 62131},
+    )
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_video_recommend(client: TestClient):
+    response = client.get("video_recommend")
+    assert response.status_code == 200
+    assert response.json()["list"]
+
+
+def test_video_dynamic(client: TestClient):
+    response = client.get("video_dynamic")
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_video_ranking(client: TestClient):
+    response = client.get("video_ranking")
+    assert response.status_code == 200
+    assert response.json()["rank"]
+
+
+def test_user_info(client: TestClient):
+    response = client.get("user_info", params={"uid": 2})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_user_uploaded(client: TestClient):
+    response = client.get("user_uploaded", params={"uid": 2})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_user_favorite(client: TestClient):
+    # TODO:add test case
+    pass
+
+
+def test_season_info(client: TestClient):
+    response = client.get("season_info", params={"season_id": 425})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_season_recommend(client: TestClient):
+    response = client.get("season_recommend", params={"season_id": 425})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_season_episode(client: TestClient):
+    response = client.get("season_episode", params={"episode_id": 84340})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_season_timeline(client: TestClient):
+    response = client.get("season_timeline")
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_season_ranking(client: TestClient):
+    response = client.get("season_ranking")
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_search(client: TestClient):
+    response = client.get("search", params={"keyword": "railgun"})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_search_recommend(client: TestClient):
+    response = client.get("search_recommend")
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_search_suggestion(client: TestClient):
+    response = client.get("search_suggestion", params={"keyword": "paperclip"})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
+
+
+def test_comments(client: TestClient):
+    response = client.get("comments", params={"id": 2})
+    assert response.status_code == 200
+    assert response.json()["code"] == 0
--- test/test_pixiv.py
+++ test/test_pixiv.py
@@ -5,45 +5,45 @@ from app import app as APIAppRoot
 from fastapi.testclient import TestClient
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="package")
 def client():
-    with TestClient(APIAppRoot) as client:
+    with TestClient(APIAppRoot, base_url="http://testserver/pixiv/") as client:
         yield client
 
 
 def test_illust(client: TestClient):
     # https://www.pixiv.net/artworks/86742914
-    response = client.get("/pixiv/illust", params={"id": 86742914})
+    response = client.get("illust", params={"id": 86742914})
     assert response.status_code == 200
     assert response.json().get("illust")
 
 
 def test_member(client: TestClient):
-    response = client.get("/pixiv/member", params={"id": 3036679})
+    response = client.get("member", params={"id": 3036679})
     assert response.status_code == 200
     assert response.json().get("user")
 
 
 def test_member_illust(client: TestClient):
-    response = client.get("/pixiv/member_illust", params={"id": 3036679})
+    response = client.get("member_illust", params={"id": 3036679})
     assert response.status_code == 200
     assert response.json().get("illusts") is not None
 
 
 def test_favorite(client: TestClient):
     # TODO: add test case
-    response = client.get("/pixiv/favorite", params={"id": 3036679})
+    response = client.get("favorite", params={"id": 3036679})
     assert response.status_code == 200
 
 
 def test_following(client: TestClient):
-    response = client.get("/pixiv/following", params={"id": 3036679})
+    response = client.get("following", params={"id": 3036679})
     assert response.status_code == 200
     assert response.json().get("user_previews") is not None
 
 
 def test_follower(client: TestClient):
-    response = client.get("/pixiv/follower", params={"id": 3036679})
+    response = client.get("follower", params={"id": 3036679})
     assert response.status_code == 200
     assert response.json().get("user_previews") is not None
 
@@ -51,31 +51,31 @@ def test_follower(client: TestClient):
 def test_rank(client: TestClient):
     for i in range(2, 5):
         response = client.get(
-            "/pixiv/rank", params={"date": str(date.today() - timedelta(days=i))}
+            "rank", params={"date": str(date.today() - timedelta(days=i))}
         )
         assert response.status_code == 200
         assert response.json().get("illusts")
 
 
 def test_search(client: TestClient):
-    response = client.get("/pixiv/search", params={"word": "東方Project"})
+    response = client.get("search", params={"word": "東方Project"})
     assert response.status_code == 200
     assert response.json().get("illusts")
 
 
 def test_tags(client: TestClient):
-    response = client.get("/pixiv/tags")
+    response = client.get("tags")
     assert response.status_code == 200
     assert response.json().get("trend_tags")
 
 
 def test_related(client: TestClient):
-    response = client.get("/pixiv/related", params={"id": 85162550})
+    response = client.get("related", params={"id": 85162550})
     assert response.status_code == 200
     assert response.json().get("illusts")
 
 
 def test_ugoira_metadata(client: TestClient):
-    response = client.get("/pixiv/ugoira_metadata", params={"id": 85162550})
+    response = client.get("ugoira_metadata", params={"id": 85162550})
     assert response.status_code == 200
     assert response.json().get("ugoira_metadata")
