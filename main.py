2d9208c11b869edd9f625937476471cd992ee34a
mix <admin@yami.im>
2021-01-09 14:07:40 +0800

    :loud_sound: 优化日志格式

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+from os import get_terminal_size
+
 import uvicorn  # type:ignore
 
 from app import app as AppRoot
 from utils.config import Config
 from utils.log import LOG_LEVEL, logger
 
-COPYRIGHT = r"""<g><b>
-  _    _   _   _       _              _____    _____ 
- | |  | | (_) | |     (_)     /\     |  __ \  |_   _|
- | |__| |  _  | |__    _     /  \    | |__) |   | |  
- |  __  | | | | '_ \  | |   / /\ \   |  ___/    | |  
- | |  | | | | | |_) | | |  / ____ \  | |       _| |_ 
- |_|  |_| |_| |_.__/  |_| /_/    \_\ |_|      |_____|
-</b></g>
-An alternative implement of Imjad API"""  # noqa:W291,W293
+COPYRIGHT = r"""<b><g>
+  _    _ _ _     _          _____ _____ 
+ | |  | (_) |   (_)   /\   |  __ \_   _|
+ | |__| |_| |__  _   /  \  | |__) || |  
+ |  __  | | '_ \| | / /\ \ |  ___/ | |  
+ | |  | | | |_) | |/ ____ \| |    _| |_ 
+ |_|  |_|_|_.__/|_/_/    \_\_|   |_____|
+</g><e>
+An alternative implement of Imjad API
+Project: https://github.com/mixmoe/HibiAPI
+</e></b>"""  # noqa:W291,W293
+
 DEBUG: bool = Config["debug"].as_bool()
 
 if __name__ == "__main__":
+    width, height = get_terminal_size()
+    logger.warning("\n".join(i.center(width) for i in COPYRIGHT.splitlines()))
     if DEBUG:
         logger.warning("<b>Server is running under <r>debug</r> mode!</b>")
-    logger.warning(COPYRIGHT)
     uvicorn.run(
         AppRoot,
         host=Config["server"]["host"].as_str(),
