b904be52c09c52fc746e83f170b6721e8d9ad9b9
mix <admin@yami.im>
2021-01-23 00:32:33 +0800

    :art: :children_crossing: 为main.py添加命令行参数

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from os import get_terminal_size
 
+import click
 import uvicorn  # type:ignore
 
-from app import app as AppRoot
+from app import app as AppRoot  # noqa:F401
 from utils.config import DEBUG, VERSION, Config
 from utils.log import LOG_LEVEL, logger
 
@@ -18,13 +19,58 @@ An alternative implement of Imjad API
 Project: https://github.com/mixmoe/HibiAPI
 </e></b>"""  # noqa:W291,W293
 
+LOG_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "default": {
+            "class": "utils.log.LoguruHandler",
+        },
+    },
+    "loggers": {
+        "uvicorn.error": {
+            "handlers": ["default"],
+            "level": LOG_LEVEL,
+        },
+        "uvicorn.access": {
+            "handlers": ["default"],
+            "level": LOG_LEVEL,
+        },
+    },
+}
+
 try:
     width, height = get_terminal_size()
 except OSError:
     width, height = 0, 0
 
 
-if __name__ == "__main__":
+@click.command(name="HibiAPI")
+@click.option(
+    "--host",
+    "-h",
+    default=Config["server"]["host"].as_str(),
+    help="listened server address",
+    show_default=True,
+)
+@click.option(
+    "--port",
+    "-p",
+    default=Config["server"]["port"].as_number(),
+    help="listened server port",
+    show_default=True,
+)
+@click.option(
+    "--workers", "-w", default=1, help="amount of server workers", show_default=True
+)
+@click.option(
+    "--reload",
+    "-r",
+    default=DEBUG,
+    help="automatic reload while file changes",
+    show_default=True,
+)
+def main(host: str, port: int, workers: int, reload: bool):
     logger.warning("\n".join(i.center(width) for i in COPYRIGHT.splitlines()))
     logger.info("HibiAPI version: <g><b>%s</b></g>" % VERSION)
     logger.info(
@@ -32,28 +78,16 @@ if __name__ == "__main__":
         % ("<r>debug</r>" if DEBUG else "<g>production</g>")
     )
     uvicorn.run(
-        AppRoot,
-        host=Config["server"]["host"].as_str(),
-        port=Config["server"]["port"].as_number(),
+        "app:app",
+        host=host,
+        port=port,
         debug=DEBUG,
         access_log=False,
-        log_config={
-            "version": 1,
-            "disable_existing_loggers": False,
-            "handlers": {
-                "default": {
-                    "class": "utils.log.LoguruHandler",
-                },
-            },
-            "loggers": {
-                "uvicorn.error": {
-                    "handlers": ["default"],
-                    "level": LOG_LEVEL,
-                },
-                "uvicorn.access": {
-                    "handlers": ["default"],
-                    "level": LOG_LEVEL,
-                },
-            },
-        },
+        log_config=LOG_CONFIG,
+        workers=workers,
+        reload=reload,
     )
+
+
+if __name__ == "__main__":
+    main()
