ab67b347d0ecab8457f572390795be609e6737a5
mix <admin@yami.im>
2021-02-13 11:12:58 +0800

    :rewind: 重新启用Pixiv API接口

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,8 +3,7 @@ from utils.routing import SlashRouter
 
 from .bilibili import router as BilibiliRouter
 from .netease import router as NeteaseRouter
-
-# from .pixiv import router as PixivRouter
+from .pixiv import router as PixivRouter
 from .qrcode import router as QRCodeRouter
 from .sauce import router as SauceRouter
 from .tieba import router as TiebaRouter
@@ -17,7 +16,7 @@ router = SlashRouter(
         for code in (400, 422, 500, 502)
     }
 )
-# router.include_router(PixivRouter, prefix="/pixiv")
+router.include_router(PixivRouter, prefix="/pixiv")
 router.include_router(BilibiliRouter, prefix="/bilibili")
 router.include_router(QRCodeRouter, prefix="/qrcode")
 router.include_router(NeteaseRouter, prefix="/netease")
