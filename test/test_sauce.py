a7ec3c4d5113af79d1cc498546ef804da25c4ffb
mix <admin@yami.im>
2021-06-22 12:27:51 +0800

    :white_check_mark: 优化SauceNAO测试使达到调用限制时自动跳过

--- test/test_sauce.py
+++ test/test_sauce.py
@@ -18,11 +18,15 @@ def client():
 def test_sauce_url(client: TestClient):
     response = client.get("sauce/", params={"url": REMOTE_SAUCE_URL})
     assert response.status_code == 200
-    assert response.json()["header"]["status"] == 0, response.json()
+    if (data := response.json())["header"]["status"] == -2:
+        pytest.skip(data["header"]["message"])
+    assert data["header"]["status"] == 0, response.json()
 
 
 def test_sauce_file(client: TestClient):
     with open(LOCAL_SAUCE_PATH, "rb") as file:
         response = client.post("sauce/", files={"file": file})
     assert response.status_code == 200
-    assert response.json()["header"]["status"] == 0, response.json()
+    if (data := response.json())["header"]["status"] == -2:
+        pytest.skip(data["header"]["message"])
+    assert data["header"]["status"] == 0, response.json()
