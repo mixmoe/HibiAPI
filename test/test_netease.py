f3b35c2ab33bd3f84c48306ebe053e13ddcaac96
Mix <32300164+mnixry@users.noreply.github.com>
2021-12-21 22:54:36 +0800

    :white_check_mark: 补充网易云音乐测试用例

--- test/test_netease.py
+++ test/test_netease.py
@@ -13,7 +13,10 @@ def client():
 def test_search(client: TestClient):
     response = client.get("search", params={"s": "test"})
     assert response.status_code == 200
-    assert response.json()["code"] == 200
+
+    data = response.json()
+    assert data["code"] == 200
+    assert data["result"]["songs"]
 
 
 def test_artist(client: TestClient):
@@ -34,10 +37,28 @@ def test_detail(client: TestClient):
     assert response.json()["code"] == 200
 
 
+def test_detail_multiple(client: TestClient):
+    response = client.get("detail", params={"id": [657666, 657667]})
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data["code"] == 200
+    assert len(data["songs"]) == 2
+
+
 def test_song(client: TestClient):
     response = client.get("song", params={"id": 657666})
     assert response.status_code == 200
     assert response.json()["code"] == 200
+
+
+def test_song_multiple(client: TestClient):
+    response = client.get("song", params={"id": [657666, 657667]})
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data["code"] == 200
+    assert len(data["data"]) == 2
 
 
 def test_playlist(client: TestClient):
