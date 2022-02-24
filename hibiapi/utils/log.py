bba5acf0e75440b10d97962b7b7009ec30a63854
Mix <32300164+mnixry@users.noreply.github.com>
2022-02-24 22:55:03 +0800

    :wrench: :loud_sound: 添加控制日志写入到文件的配置项目

--- hibiapi/utils/log.py
+++ hibiapi/utils/log.py
@@ -3,7 +3,8 @@ import re
 import sys
 from asyncio.log import logger as _asyncio_logger
 from datetime import timedelta
-from typing import TYPE_CHECKING
+from pathlib import Path
+from typing import TYPE_CHECKING, Optional
 
 import sentry_sdk.integrations.logging as sentry
 from loguru import logger as _logger
@@ -14,36 +15,39 @@ if TYPE_CHECKING:
     from loguru import Logger
 
 
-LOG_PATH = DATA_PATH / "logs"
-LOG_PATH.mkdir(parents=True, exist_ok=True)
+LOG_FILE = Config["log"]["level"].get(Optional[Path])  # type: ignore
 LOG_FORMAT = Config["log"]["format"].as_str().strip()
-LOG_LEVEL = Config["log"]["level"].get(str).upper()
+LOG_LEVEL = Config["log"]["level"].as_str().upper()
 
 
 logger: "Logger" = _logger.opt(colors=True)
 logger.remove()
 logger.add(
-    LOG_PATH / "{time}.log",
-    level=LOG_LEVEL,
-    encoding="utf-8",
-    enqueue=True,
-    compression="zip",
-    rotation=timedelta(days=1),
-)
-logger.add(
     sys.stdout,
     level=LOG_LEVEL,
     format=LOG_FORMAT,
-    filter=lambda record: record["level"].no < 30,
+    filter=lambda record: record["level"].no < logging.WARNING,
 )
 logger.add(
     sys.stderr,
     level=LOG_LEVEL,
-    filter=lambda record: record["level"].no >= 30,
+    filter=lambda record: record["level"].no >= logging.WARNING,
     format=LOG_FORMAT,
 )
 logger.add(sentry.BreadcrumbHandler(), level=LOG_LEVEL)
 logger.add(sentry.EventHandler(), level="ERROR")
+
+if LOG_FILE is not None:
+    LOG_PATH = LOG_FILE if LOG_FILE.is_absolute() else (DATA_PATH / LOG_FILE)
+    LOG_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+    logger.add(
+        LOG_PATH / LOG_FILE,
+        level=LOG_LEVEL,
+        encoding="utf-8",
+        rotation=timedelta(days=1),
+    )
+
 logger.level(LOG_LEVEL)
 
 
