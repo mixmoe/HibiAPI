e2e7bdd7354e8479d271d501bcef3d3181a8237f
Mix <32300164+mnixry@users.noreply.github.com>
2022-03-15 00:14:20 +0800

    :recycle: 使用`cashews`重构缓存组件

--- hibiapi/utils/cache.py
+++ hibiapi/utils/cache.py
@@ -1,16 +1,9 @@
-import base64
 import hashlib
-import pickle
-import pickletools
-import time
-import zlib
-from datetime import timedelta
+from datetime import datetime, timedelta
 from functools import wraps
 from typing import Any, Awaitable, Callable, Dict, Optional, Tuple, TypeVar, cast
 
-from aiocache import Cache as AioCache  # type:ignore
-from aiocache.base import BaseCache  # type:ignore
-from aiocache.serializers import BaseSerializer  # type:ignore
+from cashews import Cache
 from pydantic import BaseModel
 from pydantic.decorator import ValidatedFunction
 
@@ -26,26 +19,15 @@ CACHE_URI = Config["cache"]["uri"].as_str()
 AsyncFunc = Callable[..., Awaitable[Any]]
 T_AsyncFunc = TypeVar("T_AsyncFunc", bound=AsyncFunc)
 
+cache = Cache(name="hibiapi")
 
-class GZippedBase85Serializer(BaseSerializer):
-    def dumps(self, value: Any) -> str:
-        return base64.b85encode(
-            zlib.compress(
-                pickletools.optimize(
-                    pickle.dumps(value, protocol=pickle.HIGHEST_PROTOCOL)
-                ),
-                level=zlib.Z_BEST_COMPRESSION,
-            ),
-        ).decode()
-
-    def loads(self, value: Optional[str]) -> Any:
-        return (
-            pickle.loads(
-                zlib.decompress(base64.b85decode(value)),
-            )
-            if value
-            else None
-        )
+try:
+    cache.setup(CACHE_URI)
+except Exception as e:
+    logger.warning(
+        f"Cache URI <y>{CACHE_URI!r}</y> setup <r><b>failed</b></r>: "
+        f"<r>{e!r}</r>, use memory backend instead."
+    )
 
 
 class CacheConfig(BaseModel):
@@ -99,55 +81,45 @@ def endpoint_cache(function: T_AsyncFunc) -> T_AsyncFunc:
     from .routing import request_headers, response_headers
 
     vf = CachedValidatedFunction(function, config={})
-    cache = cast(BaseCache, AioCache.from_url(CACHE_URI))
     config = cast(
         CacheConfig,
         getattr(function, CACHE_CONFIG_KEY, None) or CacheConfig.new(function),
     )
 
-    cache.namespace, cache.ttl = config.namespace, config.ttl.total_seconds()
-    cache.serializer = GZippedBase85Serializer(encoding="utf-8")
-
-    if not CACHE_ENABLED:
-        config.enabled = False
+    config.enabled = CACHE_ENABLED and config.enabled
 
     @wraps(function)
     async def wrapper(*args, **kwargs):
         cache_policy: str = request_headers.get().get("cache-control", "public")
 
-        if (
-            not config.enabled
-            or cache.ttl is None
-            or cache_policy.casefold() == "no-store"
-        ):
+        if not config.enabled or cache_policy.casefold() == "no-store":
             return await vf.call(*args, **kwargs)
 
-        key = hashlib.md5(
-            (model := vf.serialize(args=args, kwargs=kwargs))
-            .json(exclude={"self"}, sort_keys=True, ensure_ascii=False)
-            .encode()
-        ).hexdigest()
+        key = (
+            f"{config.namespace}:"
+            + hashlib.md5(
+                (model := vf.serialize(args=args, kwargs=kwargs))
+                .json(exclude={"self"}, sort_keys=True, ensure_ascii=False)
+                .encode()
+            ).hexdigest()
+        )
 
         response_header = response_headers.get()
-        cache_result: Optional[Tuple[Any, float]] = None
+        result: Optional[Tuple[Any, datetime]] = None
 
         if cache_policy.casefold() == "no-cache":
             await cache.delete(key)
-        elif cache_result := await cache.get(key):
-            logger.debug(f"Request hit cache <e>{cache.namespace}</e>:<g>{key}</g>")
+        elif result := await cache.get(key):
+            logger.debug(f"Request hit cache <b><e>{key}</e></b>")
             response_header.setdefault("X-Cache-Hit", key)
 
-        now_time = time.time()
+        if result is None:
+            result = await vf.execute(model)
+            result = await cache.set(key, result, expire=config.ttl)
 
-        if cache_result is None:
-            cache_result = await vf.execute(model), now_time
-            await cache.set(key, cache_result)
-
-        return_result, cache_time = cache_result
-
-        if (cache_remain := int(cache_time + cache.ttl - now_time)) > 0:
+        if (cache_remain := await cache.get_expire(key)) > 0:
             response_header.setdefault("Cache-Control", f"max-age={cache_remain}")
 
-        return return_result
+        return result
 
     return wrapper  # type:ignore
