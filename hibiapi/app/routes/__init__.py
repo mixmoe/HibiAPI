5cb852e3108bc2c0d5faad69fc3e19f37011bf54
Mix <32300164+mnixry@users.noreply.github.com>
2022-10-31 23:10:28 +0800

    :sparkles: 完成哔咔API的请求路由支持

--- hibiapi/app/routes/__init__.py
+++ hibiapi/app/routes/__init__.py
@@ -1,11 +1,19 @@
 from typing import List, Protocol, cast
 
+from hibiapi.app.routes import (
+    bika,
+    bilibili,
+    netease,
+    pixiv,
+    qrcode,
+    sauce,
+    tieba,
+    wallpaper,
+)
 from hibiapi.utils.config import APIConfig
 from hibiapi.utils.exceptions import ExceptionReturn
 from hibiapi.utils.log import logger
 from hibiapi.utils.routing import SlashRouter
-
-from . import bilibili, netease, pixiv, qrcode, sauce, tieba, wallpaper
 
 router = SlashRouter(
     responses={
@@ -24,15 +32,17 @@ class RouteInterface(Protocol):
 
 
 modules = cast(
-    List[RouteInterface], [bilibili, netease, pixiv, qrcode, sauce, tieba, wallpaper]
+    List[RouteInterface],
+    [bilibili, netease, pixiv, qrcode, sauce, tieba, wallpaper, bika],
 )
 
 for module in modules:
     mount = (
-        mountpoint
-        if (mountpoint := module.__mount__).startswith("/")
-        else ("/" + mountpoint)
+        mount_point
+        if (mount_point := module.__mount__).startswith("/")
+        else f"/{mount_point}"
     )
+
     if not module.__config__["enabled"].as_bool():
         logger.warning(
             f"API Route <y><b>{mount}</b></y> has been <r><b>disabled</b></r> in config."
